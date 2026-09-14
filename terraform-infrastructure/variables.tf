c339f5c61f950991b520b7505da6fc118c1c3b83
dariusandrei10 <120843191+dariusandrei10@users.noreply.github.com>
2026-09-14 04:17:03 +0300

    Rename variable 'app_name' to 'app'
    Renamed variable 'app_name' to 'app' for clarity.

--- terraform-infrastructure/variables.tf
+++ terraform-infrastructure/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   default     = "westeurope"
 }
 
-variable "app_name" {
+variable "app" {
   description = "Name of the application, used in Azure resource naming"
   type        = string
   default     = "podtok"
